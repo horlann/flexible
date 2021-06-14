3b538798b0937f1397b29e6eb53b7deaa7fee54a
Home <digiborimusik@gmail.com>
2021-06-14 16:45:27 +0300

    deadline chooser

--- lib/board/task_editor/deadline_chooser.dart
+++ lib/board/task_editor/deadline_chooser.dart
@@ -1,3 +1,4 @@
+import 'package:flexible/board/calendar_dialog.dart';
 import 'package:flexible/utils/adaptive_utils.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,37 @@ class DeadLineChooser extends StatelessWidget {
     required this.onChange,
   }) : super(key: key);
 
+  Duration get deadlineDuration {
+    return initialDeadline.difference(timeStart);
+  }
+
+  void showCalendar(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return CalendarDialog(
+            focusedDay: DateTime.now(),
+            onSelect: (date) {
+              Navigator.pop(context);
+              print(date);
+              onChange(date);
+            });
+      },
+    );
+  }
+
+  bool get isCustomDeadline {
+    if (deadlineDuration.inDays == 7) {
+      return false;
+    } else if (deadlineDuration.inDays == 14) {
+      return false;
+    } else if (deadlineDuration.inDays == 30) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -19,26 +51,34 @@ class DeadLineChooser extends StatelessWidget {
       children: [
         buildButton(context,
             text: 'Week',
+            active: deadlineDuration.inDays == 7 ? true : false,
             callback: () => onChange(timeStart.add(Duration(days: 7)))),
         buildButton(context,
             text: '2 Weeks',
+            active: deadlineDuration.inDays == 14 ? true : false,
             callback: () => onChange(timeStart.add(Duration(days: 14)))),
         buildButton(context,
             text: 'Month',
+            active: deadlineDuration.inDays == 30 ? true : false,
             callback: () => onChange(timeStart.add(Duration(days: 30)))),
-        buildButton(context, text: 'Choose', callback: () {
+        buildButton(context,
+            active: isCustomDeadline,
+            text: isCustomDeadline
+                ? initialDeadline.toString().substring(0, 10)
+                : 'Choose', callback: () {
           // todo
+          showCalendar(context);
         }),
       ],
     );
   }
 
   ClipRRect buildButton(BuildContext context,
-      {required String text, required Function callback}) {
+      {required String text, bool active = false, required Function callback}) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(4),
       child: Material(
-        color: Colors.white,
+        color: active ? Color(0xffE24F4F) : Colors.white,
         child: InkWell(
           onTap: () => callback(),
           child: Container(
@@ -46,7 +86,10 @@ class DeadLineChooser extends StatelessWidget {
                 vertical: 4 * byWithScale(context),
                 horizontal: 6 * byWithScale(context)),
             child: Text(text,
-                style: TextStyle(fontSize: 10 * byWithScale(context))),
+                style: TextStyle(
+                  fontSize: 10 * byWithScale(context),
+                  color: !active ? Color(0xffE24F4F) : Colors.white,
+                )),
           ),
         ),
       ),
