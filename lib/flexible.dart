f9fc894bf6438978cf408d301b1d7956a2de4eb2
Home <digiborimusik@gmail.com>
2021-06-30 14:10:07 +0300

    chore

--- lib/flexible.dart
+++ lib/flexible.dart
@@ -1,12 +1,6 @@
-import 'package:flexible/authentification/auth_pusher.dart';
-import 'package:flexible/authentification/auth_wrapper.dart';
 import 'package:flexible/authentification/bloc/auth_bloc.dart';
-import 'package:flexible/authentification/code_verification_page.dart';
 import 'package:flexible/authentification/services/firebase_auth.dart';
-import 'package:flexible/authentification/registration_page.dart';
 import 'package:flexible/authentification/services/users_data_repository.dart';
-import 'package:flexible/authentification/sign_in_page.dart';
-import 'package:flexible/board/board_page.dart';
 import 'package:flexible/board/bloc/dailytasks_bloc.dart';
 import 'package:flexible/board/repository/combined_repository/combined_tasks_repo.dart';
 import 'package:flexible/board/repository/firebaseRepository/fire_days_options.dart';
@@ -16,8 +10,7 @@ import 'package:flexible/board/repository/sqFliteRepository/sqflire_tasks.dart';
 import 'package:flexible/board/repository/sqFliteRepository/sqflite_day_options.dart';
 import 'package:flexible/helper/helper_wrapper.dart';
 import 'package:flexible/subscription/bloc/subscribe_bloc.dart';
-import 'package:flexible/subscription/subscribe_page.dart';
-import 'package:flexible/utils/main_backgroung_gradient.dart';
+import 'package:flexible/subscription/subscription_wrapper.dart';
 import 'package:flexible/weather/bloc/weather_bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -77,110 +70,19 @@ class FlexibleApp extends StatelessWidget {
         ],
         child: MaterialApp(
           theme: ThemeData(fontFamily: 'Mikado'),
+          // Force app to minimize on last pop
           home: WillPopScope(
             onWillPop: () async {
               MoveToBackground.moveTaskToBack();
               return false;
             },
+            // Helper is showing on first app start and never again
             child: HelperWrapper(
               child: SubAndAuthChooser(),
             ),
           ),
         ),
       ),
-    );
-  }
-}
-
-class SubAndAuthChooser extends StatelessWidget {
-  const SubAndAuthChooser({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<SubscribeBloc, SubscribeState>(
-      listener: (context, state) {
-        if (state is RegisterAndProcess) {
-          // Push pre registration
-          // continue process if user authed by state bools
-          // or abort if user out from auth
-          Navigator.push(
-              context,
-              CupertinoPageRoute(
-                builder: (context) => AuthBlocPusher(),
-              )).then((value) {
-            print(value);
-            // AuthBlocPusher return true if outh complete
-            if (value != null && value == true) {
-              // Continue what you need
-              if (state.continueSubscribe) {
-                BlocProvider.of<SubscribeBloc>(context).add(Restore());
-              }
-              if (state.continueRestore) {
-                BlocProvider.of<SubscribeBloc>(context).add(Subscribe());
-              }
-            } else {
-              // Just return and update
-              BlocProvider.of<SubscribeBloc>(context).add(Update());
-            }
-          });
-        }
-      },
-      builder: (context, state) {
-        print(state);
-        if (state is UnSubscribed) {
-          // BlocProvider.of<AuthBloc>(context).add(SignOut());
-          return BoardPage();
-        }
-
-        if (state is Subscribed) {
-          return BoardPage();
-        }
-
-        if (state is SubscribtionDeactivated) {
-          return BoardPage();
-        }
-
-        if (state is AskForSubscribe) {
-          return SubscribePage();
-        }
-
-        return Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class PaymentPage extends StatelessWidget {
-  const PaymentPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        gradient: mainBackgroundGradient,
-      ),
-      child: SafeArea(
-        child: CustomScrollView(
-          physics: BouncingScrollPhysics(),
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: true,
-              child: buildBody(context),
-            ),
-          ],
-        ),
-      ),
-    ));
-  }
-
-  Widget buildBody(BuildContext context) {
-    return Center(
-      child: Text('payment'),
     );
   }
 }
--- lib/subscription/subscription_wrapper.dart
+++ lib/subscription/subscription_wrapper.dart
@@ -1,30 +1,57 @@
+import 'package:flexible/authentification/auth_pusher.dart';
+import 'package:flexible/board/board_page.dart';
 import 'package:flexible/subscription/bloc/subscribe_bloc.dart';
 import 'package:flexible/subscription/subscribe_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SubscriptionWrapper extends StatelessWidget {
-  final Widget child;
-  const SubscriptionWrapper({
-    Key? key,
-    required this.child,
-  }) : super(key: key);
+class SubAndAuthChooser extends StatelessWidget {
+  const SubAndAuthChooser({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<SubscribeBloc, SubscribeState>(
+    return BlocConsumer<SubscribeBloc, SubscribeState>(
+      listener: (context, state) {
+        if (state is RegisterAndProcess) {
+          // Push pre registration
+          // continue process if user authed by state bools
+          // or abort if user out from auth
+          Navigator.push(
+              context,
+              CupertinoPageRoute(
+                builder: (context) => AuthBlocPusher(),
+              )).then((value) {
+            print(value);
+            // AuthBlocPusher return true if outh complete
+            if (value != null && value == true) {
+              // Continue what you need
+              if (state.continueSubscribe) {
+                BlocProvider.of<SubscribeBloc>(context).add(Restore());
+              }
+              if (state.continueRestore) {
+                BlocProvider.of<SubscribeBloc>(context).add(Subscribe());
+              }
+            } else {
+              // Just return and update
+              BlocProvider.of<SubscribeBloc>(context).add(Update());
+            }
+          });
+        }
+      },
       builder: (context, state) {
         print(state);
         if (state is UnSubscribed) {
-          return child;
+          // BlocProvider.of<AuthBloc>(context).add(SignOut());
+          return BoardPage();
         }
 
         if (state is Subscribed) {
-          return child;
+          return BoardPage();
         }
 
         if (state is SubscribtionDeactivated) {
-          return child;
+          return BoardPage();
         }
 
         if (state is AskForSubscribe) {
